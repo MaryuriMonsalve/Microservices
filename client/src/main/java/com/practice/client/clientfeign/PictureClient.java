46c0349eb99950c4e19229288a61f43676c72036
MaryuriMonsalve <maryurimonsalve95@gmail.com>
2021-06-26 23:14:51 -0500

    Hystrix

--- client/src/main/java/com/practice/client/clientfeign/PictureClient.java
+++ client/src/main/java/com/practice/client/clientfeign/PictureClient.java
@@ -2,7 +2,6 @@ package com.practice.client.clientfeign;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="picture", fallback = PictureHystrixFallBackFactory.class)
--- client/src/main/java/com/practice/client/controller/ClientRest.java
+++ client/src/main/java/com/practice/client/controller/ClientRest.java
@@ -1,6 +1,6 @@
 package com.practice.client.controller;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 import com.practice.client.dto.ClientDto;
 import com.practice.client.service.IClientService;
 import org.springframework.web.bind.annotation.*;
@@ -17,7 +17,7 @@ public class ClientRest {
     public ClientRest(IClientService clientService){
         this.clientService = clientService;
     }
-    
+
     @PostMapping("/save")
     public ClientDto save(@RequestBody ClientDto cliente)throws Exception{
         return clientService.save(cliente);
