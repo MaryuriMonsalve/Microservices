a354945f903db14e9d3700edaee06e3d322617ee
MaryuriMonsalve <maryurimonsalve95@gmail.com>
2021-06-26 22:41:33 -0500

    Hystrix

--- client/src/main/java/com/practice/client/clientfeign/PictureHystrixFallBackFactory.java
+++ client/src/main/java/com/practice/client/clientfeign/PictureHystrixFallBackFactory.java
@@ -1,5 +1,8 @@
 package com.practice.client.clientfeign;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class PictureHystrixFallBackFactory implements PictureClient{
     @Override
     public String procesarFoto(String fileBase64) {
